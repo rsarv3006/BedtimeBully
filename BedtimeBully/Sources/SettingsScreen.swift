7731314ff6b653173e86dc4ce16d4e3296b52ed2
Robby Sarvis <rjs.app.dev@gmail.com>
2024-05-04 20:50:00 -0500

    settings screen max width

--- BedtimeBully/Sources/SettingsScreen.swift
+++ BedtimeBully/Sources/SettingsScreen.swift
@@ -5,8 +5,8 @@
 //  Created by Robert J. Sarvis Jr on 12/18/23.
 //
 
-import SwiftUI
 import Notifications
+import SwiftUI
 
 struct SettingsScreen: View {
     var body: some View {
@@ -15,14 +15,16 @@ struct SettingsScreen: View {
                 VStack {
                     Text("[Contact Support](https://rjsappdev.wixsite.com/bedtime-bully/general-5)")
                         .modifier(RoundedBorderView())
-                    
+                        .frame(maxWidth: 350)
+
                     Text("[Privacy Policy](https://rjsappdev.wixsite.com/bedtime-bully/privacy-policy)")
                         .modifier(RoundedBorderView())
-                    
+                        .frame(maxWidth: 350)
+
                     Text("[EULA](https://rjsappdev.wixsite.com/bedtime-bully/eula)")
                         .modifier(RoundedBorderView())
+                        .frame(maxWidth: 350)
 
-                    
                     HStack {
                         Spacer()
                     }
@@ -37,5 +39,5 @@ struct SettingsScreen: View {
 }
 
 #Preview {
-    return SettingsScreen()
+    SettingsScreen()
 }
