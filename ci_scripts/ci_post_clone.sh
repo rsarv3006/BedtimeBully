282848410b6424083788955d7b826c2b93fc1beb
Robby Sarvis <rjs.app.dev@proton.me>
2024-10-10 16:08:11 -0500

    more ci script tweaks

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,12 +5,7 @@ export PATH="$HOME/.local/bin/$PATH" # Installs the tools in .mise.toml in the p
 ~/.local/bin/mise --version
 ~/.local/bin/mise install # Installs the version from .mise.toml
 
-if [ "$CI" ]; then
-     echo "Skip shims due to CI"
-else
-     echo "Activating shims for local dev"
-     eval "$(mise activate bash --shims)" # activate shims to enable local use of mise
-fi
+eval "$(mise activate bash --shims)" # activate shims to enable local use of mise
 
 mise doctor # verify the output of mise is correct on CI
 
